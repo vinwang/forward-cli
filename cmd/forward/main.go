1f081e2d574a3c969e1f188f6eb2abd9e06ad00c
wanglu <525824675@qq.com>
2023-04-17 15:50:34 +0800

    添加获取外网ip

--- cmd/forward/main.go
+++ cmd/forward/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
@@ -32,6 +33,22 @@ func getLocalIP() net.IP {
 	localAddr := conn.LocalAddr().(*net.UDPAddr)
 
 	return localAddr.IP
+}
+
+func getIP() (string, error) {
+
+	responseClient, errClient := http.Get("http://ip.dhcp.cn/?ip") // 获取外网 IP
+	if errClient != nil {
+		fmt.Printf("获取外网 IP 失败，请检查网络\n")
+		panic(errClient)
+	}
+	// 程序在使用完 response 后必须关闭 response 的主体。
+	defer responseClient.Body.Close()
+
+	body, _ := ioutil.ReadAll(responseClient.Body)
+	clientIP := fmt.Sprintf("%s", string(body))
+
+	return clientIP, nil
 }
 
 func printHelp() {
